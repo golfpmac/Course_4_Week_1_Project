cbd42f6bf4963cb0c7a96c82738704c994c5777a
golfpmac <pmcpherson@hotmail.com>
2019-09-10 11:29:06 -0700

    Final

---
+++ Plot3.R
@@ -0,0 +1,25 @@
+require(sqldf)
+library(sqldf)
+
+#loads file path and filename
+filename = "C:\\Users\\Patrick\\datasciencecoursera\\Course 4\\household_power_consumption.txt"
+fi = file(filename)
+
+#loads file into dataframe.  Used SQL to limit rows loaded to necessary dates
+df = sqldf("select * from fi where Date = '1/2/2007' or Date = '2/2/2007'", 
+           file.format = list(header = TRUE, sep = ";"))
+
+#Close SQL connection
+close(fi)
+
+#Combine Date & time into one field
+df$DateTime = with(df, paste(Date, Time))
+df = df[, -c(1:2)]
+
+#convert DateTime from char
+df$DateTime = strptime(df$DateTime, "%d/%m/%Y %H:%M:%S")
+
+#create plot & save to png file
+png("plot2.png", width = 480, height = 480, units = "px")
+plot(df$DateTime, df$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts", xlab = "")
+dev.off()
